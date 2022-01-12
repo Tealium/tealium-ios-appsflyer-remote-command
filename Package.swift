3c10d636074fa0aa16704758da32d35b17c0f338
Enrico Zannini <enricozannini93@gmail.com>
2022-01-12 12:24:50 +0100

    SPM versions up to next major

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -10,16 +10,16 @@ let package = Package(
         .library(name: "TealiumAppsFlyer", targets: ["TealiumAppsFlyer"])
     ],
     dependencies: [
-        .package(url: "https://github.com/tealium/tealium-swift", from: "2.6.0"),
-        .package(url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", from: "6.4.0")
+        .package(name: "TealiumSwift", url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.6.0")),
+        .package(name: "AppsFlyerLib", url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", .upToNextMajor(from: "6.4.0"))
     ],
     targets: [
         .target(
             name: "TealiumAppsFlyer",
             dependencies: [
-                .product(name: "TealiumCore", package: "tealium-swift"),
-                .product(name: "TealiumRemoteCommands", package: "tealium-swift"),
-                .product(name: "AppsFlyerLib", package: "AppsFlyerFramework")
+                .product(name: "TealiumCore", package: "TealiumSwift"),
+                .product(name: "TealiumRemoteCommands", package: "TealiumSwift"),
+                .product(name: "AppsFlyerLib", package: "AppsFlyerLib")
             ],
             path: "./Sources",
             exclude: ["Support"]),
