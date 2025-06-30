94d104b41f69d150ac2370bcf8bf666488526fd5
Sebastian Krajna <sebastian.krajna@tealium.com>
2025-06-30 11:45:30 +0200

    Update dependencies and versioning for TealiumAppsFlyer integration
    - Bump TealiumSwift to version 2.18 and AppsFlyerFramework to version 6.17 in Cartfile and related files.
    - Update podspec version to 3.1.0 and adjust dependencies accordingly.
    - Reflect version changes in example project and Podfile.lock.
    
    This update ensures compatibility with the latest versions of the respective SDKs.

--- Package.swift
+++ Package.swift
@@ -10,9 +10,8 @@ let package = Package(
         .library(name: "TealiumAppsFlyer", targets: ["TealiumAppsFlyer"])
     ],
     dependencies: [
-        .package(name: "TealiumSwift", url: "https://github.com/tealium/tealium-swift", 
-.upToNextMajor(from: "2.12.0")),
-        .package(name: "AppsFlyerLib", url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", .upToNextMajor(from: "6.9.0"))
+        .package(name: "TealiumSwift", url: "https://github.com/tealium/tealium-swift", .upToNextMajor(from: "2.18.0")),
+        .package(name: "AppsFlyerLib", url: "https://github.com/AppsFlyerSDK/AppsFlyerFramework", .upToNextMajor(from: "6.17.0"))
     ],
     targets: [
         .target(
